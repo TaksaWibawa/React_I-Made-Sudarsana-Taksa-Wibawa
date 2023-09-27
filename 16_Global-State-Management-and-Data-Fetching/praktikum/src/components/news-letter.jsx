a3898d797b309b42debf8d4c20253d9c87145f93
I Made Sudarsana Taksa Wibawa <98959851+TaksaWibawa@users.noreply.github.com>
2023-09-27 15:15:12 +0800

    feat(global-state-management-and-data-fetching): add assignments, summary, and screenshots for this topic

---
+++ 16_Global-State-Management-and-Data-Fetching/praktikum/src/components/news-letter.jsx
@@ -0,0 +1,19 @@
+function NewsLetter() {
+	return (
+		<section className="newsletter">
+			<h2>Join Our Newsletter</h2>
+			<p>
+				Tamen quem nulla quae legam multos aute sint culpa legam noster magna
+			</p>
+			<form id="form-news">
+				<input
+					type="text"
+					name="newsletter"
+				/>
+				<button type="submit">Subscribe</button>
+			</form>
+		</section>
+	);
+}
+
+export default NewsLetter;
